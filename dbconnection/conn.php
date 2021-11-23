a6dce617f1900693b465ea4ef54eb0d4832a72e3
jehuzzz143 <Jehuzzz143@gmail.com>
2021-11-23 16:24:49 +0800

    title icon update

--- dbconnection/conn.php
+++ dbconnection/conn.php
@@ -8,6 +8,10 @@
 </style>
 <head>
   <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css"> -->
+     <link rel="apple-touch-icon" sizes="180x180" href="../title/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="../title/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="../title/favicon-16x16.png">
+<link rel="manifest" href="../title/site.webmanifest">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.1.5/dist/sweetalert2.all.min.js"></script>
     <script src="sweetalert2/dist/sweetalert2.all.min.js"></script>
 </head>
